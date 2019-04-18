f1dd3eed17266e4d35e38d18b6d842a1d1605302
anikseu <hello@anik-online.com>
2019-04-19 05:37:23 +0600

    sql files

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
- $con = mysqli_connect('localhost','alorjibon_main','SEU45BEST@@');
+ $con = mysqli_connect('localhost','username','password');
  mysqli_select_db($con, 'alorjibon_main');
  
 ?>
